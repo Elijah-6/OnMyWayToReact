95e720e050ba66c532e7da6d1a822051846025f9
elijah-6 <elijahfrimpong76@gmail.com>
2022-10-19 19:35:54 +0000

    changed from tsx to js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
     <div>
       <Head>
@@ -18,10 +17,18 @@ const Home: NextPage = () => {
      <main className='max-w-7xl mx-auto'>
       <section className='pt-5'>
         <h3 className='font-semibold text-4xl pb-5'> Explore Nearby</h3>
+        {/* {exploreData.map((item)=>(<h2>{item.location}</h2>))} */}
       </section>
      </main>
     </div>
   )
-}
+};
 
-export default Home
+
+// export async function getStaticProps() { 
+//   const exploreData = await fetch("https://links.papareact.com/pyp").then((res)=>res.json());
+//   return {
+//     props: {exploreData},
+//   };
+// }
+
